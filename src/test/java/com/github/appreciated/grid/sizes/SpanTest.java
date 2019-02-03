46ac2d47b15e957a876c7ff89b898f67990d6feb
Johannes Goebel <GoebelJohannes@gmx.net>
2019-02-03 16:15:24 +0100

    - added some more tests

--- src/test/java/com/github/appreciated/grid/sizes/SpanTest.java
+++ src/test/java/com/github/appreciated/grid/sizes/SpanTest.java
@@ -1,5 +1,6 @@
 package com.github.appreciated.grid.sizes;
 
+import com.github.appreciated.grid.exception.NegativeOrZeroValueException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,5 +10,15 @@ public class SpanTest {
         Assert.assertEquals("span 1", new Span(1).getCssValue());
         Assert.assertEquals("span 1 test", new Span(1, new Area("test")).getCssValue());
         Assert.assertEquals("span test", new Span(new Area("test")).getCssValue());
+    }
+
+    @Test(expected = NegativeOrZeroValueException.class)
+    public void constructorZeroTest() {
+        new Span(0).getCssValue();
+    }
+
+    @Test(expected = NegativeOrZeroValueException.class)
+    public void constructorNegativeTest() {
+        new Span(-1).getCssValue();
     }
 }
