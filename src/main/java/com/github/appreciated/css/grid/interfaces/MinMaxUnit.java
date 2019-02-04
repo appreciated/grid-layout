06c9ac950b7a381cb6027d69aa42f1278361a1bf
Johannes Goebel <GoebelJohannes@gmx.net>
2019-02-04 14:06:32 +0100

    - added some moar documentation

--- src/main/java/com/github/appreciated/css/grid/interfaces/MinMaxUnit.java
+++ src/main/java/com/github/appreciated/css/grid/interfaces/MinMaxUnit.java
@@ -1,7 +1,7 @@
 package com.github.appreciated.css.grid.interfaces;
 
 /**
- * This interface is implemented by the following classes to match the standard <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/minmax">@mozilla.org</a> </br>
+ * This interface is implemented by the following classes to match the following <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/minmax">min-max</a> standard </br>
  * {@link com.github.appreciated.css.grid.sizes.Auto} </br>
  * {@link com.github.appreciated.css.grid.sizes.Length} </br>
  * {@link com.github.appreciated.css.grid.sizes.Flex} </br>
